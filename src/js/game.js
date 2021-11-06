d4598c7a4fcd3d9392cf2ee0438a7dd61643f187
SazonovDmitriy <sazonovd21@mail.ru>
2021-11-06 14:29:32 +0300

    2

--- src/js/game.js
+++ src/js/game.js
@@ -1,11 +1,6 @@
 export default function order (arr) {
-let testArr = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-];
-    testArr.sort((a,b) => {
+    arr.sort((a,b) => {
         return b.health - a.health 
     }, 0);
-    return testArr
+    return arr
 }
